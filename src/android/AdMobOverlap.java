3354c82f3ab8e6e4300b1c87a87999678d33a466
joanmartinez1993 <39814011+joanmartinez1993@users.noreply.github.com>
2018-05-31 20:46:52 -0400

    Update AdMobOverlap.java

--- src/android/AdMobOverlap.java
+++ src/android/AdMobOverlap.java
@@ -1,4 +1,4 @@
-/Copyright (c) 2014 Sang Ki Kwon (Cranberrygame)
+//Copyright (c) 2014 Sang Ki Kwon (Cranberrygame)
 //Email: cranberrygame@yahoo.com
 //Homepage: http://cranberrygame.github.io
 //License: MIT (http://opensource.org/licenses/MIT)
